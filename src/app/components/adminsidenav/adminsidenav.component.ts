f9d683291a5b94f93f96bcf5bb06beb66643a099
Emon <jahidulemon49@gmail.com>
2023-09-16 18:41:22 +0600

    Admin Panel added. navbar modified

--- src/app/components/adminsidenav/adminsidenav.component.ts
+++ src/app/components/adminsidenav/adminsidenav.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import {
+  faDashboard,
+  faLocation,
+  faShop,
+  faBox,
+  faMoneyBill,
+  faChartBar,
+  faContactBook,
+  faHand,
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-adminsidenav',
   templateUrl: './adminsidenav.component.html',
   styleUrls: ['./adminsidenav.component.css']
 })
 export class AdminsidenavComponent {
-
+  faDashboard = faDashboard;
+  faLocation = faLocation;
+  faShop = faShop
+  faBox = faBox;
+  faMoneyBill =faMoneyBill
+  faChartBar = faChartBar;
+  faContactBook = faContactBook;
+  faHand = faHand;
 }
