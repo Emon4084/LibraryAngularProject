4f04b84a02600fbbc76becb0f335fd96a757d104
SM0101 <minhajsayed05@gmail.com>
2023-09-22 23:39:51 +0600

    author and book added also index file modified

--- src/app/models/book/book.model.ts
+++ src/app/models/book/book.model.ts
@@ -1,73 +1,28 @@
 import { Bookauthor } from "../bookauthor/bookauthor.model";
-import { BookCopy } from "../bookcopy/bookcopy.model";
-import { BookReview } from "../bookreview/book-review.model";
-import { BookWishlist } from "../bookwishlist/wishlist.model";
-import { Borrowedbook } from "../borrowedbook/borrowedbook.model";
-import { Category } from "../category/category.model";
-import { Publisher } from "../publisher/publisher.model";
 
-// export class Book {
-//   constructor(
-    // public BookId?: number,
-    // public Title?: string,
-    // public ISBN?: string,
-    //
-    // public PublisherId?: string,
-    // public Publisher?: Publisher,
-    // public PublishedYear?: string,
-    // public Edition?: string,
-    // public TotalCopies?: string,
-    // public Language?: string,
-    // public Description?: string,
-    // public BookPrice?: number,
-    // public RentPrice?: string,
-    // public DDCCode?: string,
-    // public Cover?: string,
-
-    // //[NotMapped] 
-    // //    public IFormFile Cover { get; set; }
-
-    // public IsDigital?: string,
-
-    // //[NotMapped]
-    // //    public IFormFile EBook { get; set; }
-
-    
-    // public PublisherAgreement?: string,
-    // //[NotMapped]
-    // //    public IFormFile AgreementCopy { get; set; }
-
-    // public CategoryId: number=0,
-    // public category?: Category
-
-    export class Book {
-      constructor(
-        public bookId: number,
-        public title: string ,
-        public isbn: string ,
-        public publisherId:number ,
-        public publisher: Publisher,
-        public publishedYear: Date,
-        public edition: string ,
-        public totalCopies: number,
-        public language: string ,
-        public description: string,
-        public bookPrice: number ,
-        public rentPrice: number ,
-        public ddcCode: string,
-        public isActive: boolean,
-        public cover: File ,
-        public isDigital: boolean,
-        public eBook: File ,
-        public publisherAgreement: boolean,
-        public agreementBookCopy: File ,
-        public categoryId: number,
-        public category: Category,
-        public bookAuthor?: Bookauthor[] ,
-        public copies?: BookCopy[] ,
-        public bookReviews?: BookReview[] ,
-        public bookWishlists?: BookWishlist[] ,
-        public borrowedBooks?: Borrowedbook[]
-      ) {}
-    }
-
+export interface Book {
+  bookId?:number;
+  title?: string;
+  isbn?: string;
+  publisherId?: number;
+  publishedYear?: Date;
+  edition?: string;
+  totalCopies?: number;
+  language?: string;
+  description?: string;
+  bookPrice?: number;
+  ddcCode?: string;
+  isActive?: boolean;
+  cover?: string;
+  isDigital?: boolean;
+  eBook?: string;
+  publisherAgreement?: boolean;
+  agreementBookCopy?: string;
+  categoryId?: number;
+  bookAuthor?: Bookauthor[];
+  authorIds?: number[];
+  authorIdsToRemove?: number[];
+  coverFileName: string;
+  eBookFileName: string;
+  agreementFileName:string;
+}
